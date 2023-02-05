f48856696bc5dda79d9af61d5eb88b6af6a16462
wanabeunique <cool.olo124@list.ru>
2023-02-06 02:11:08 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,119 @@
+function disableScroll () {
+  const pagePosition = window.scrollY;
+  const paddingOffset = `${(window.innerWidth - document.body.offsetWidth)}px`;
+
+  document.documentElement.style.scrollBehavior = 'none';
+  document.body.style.paddingRight = paddingOffset;
+  document.body.classList.add('dis-scroll');
+  document.body.dataset.position = pagePosition;
+  document.body.style.top = `-${pagePosition}px`;
+}
+
+function enableScroll () {
+  const pagePosition = parseInt(document.body.dataset.position, 10);
+  document.body.style.paddingRight = '0px';
+
+  document.body.style.top = 'auto';
+  document.body.classList.remove('dis-scroll');
+  window.scroll({
+    top: pagePosition,
+    left: 0
+  });
+  document.body.removeAttribute('data-position');
+  document.documentElement.style.scrollBehavior = 'smooth';
+}
+
+
+
+(function(){
+  const burger = document?.querySelector('[data-burger]');
+  const menu = document?.querySelector('[data-menu]');
+  const menuItems = document?.querySelectorAll('[data-menu-item]');
+  // const overlay = document?.querySelector('[data-menu-overlay]');
+
+  burger?.addEventListener('click', (e) => {
+    burger?.classList.toggle('burger--active');
+    menu?.classList.toggle('menu--active');
+
+    if (menu?.classList.contains('menu--active')) {
+      burger?.setAttribute('aria-expanded', 'true');
+      burger?.setAttribute('aria-label', 'Закрыть меню');
+      disableScroll();
+    } else {
+      burger?.setAttribute('aria-expanded', 'false');
+      burger?.setAttribute('aria-label', 'Открыть меню');
+      enableScroll();
+    }
+  });
+
+  // overlay?.addEventListener('click', () => {
+  //   burger?.setAttribute('aria-expanded', 'false');
+  //   burger?.setAttribute('aria-label', 'Открыть меню');
+  //   burger.classList.remove('burger--active');
+  //   menu.classList.remove('menu--active');
+  //   enableScroll();
+  // });
+
+  menuItems?.forEach(el => {
+    el.addEventListener('click', () => {
+      burger?.setAttribute('aria-expanded', 'false');
+      burger?.setAttribute('aria-label', 'Открыть меню');
+      burger.classList.remove('burger--active');
+      menu.classList.remove('menu--active');
+      enableScroll();
+    });
+  });
+})();
+
+
+
+
+
+// SWIPER
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  spaceBetween: 30,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+//
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+	const accordions = document.querySelectorAll('.accordion');
+
+	accordions.forEach(el => {
+		el.addEventListener('click', (e) => {
+			const self = e.currentTarget;
+			const control = self.querySelector('.accordion__control');
+			const content = self.querySelector('.accordion__content');
+
+			self.classList.toggle('open');
+
+			// если открыт аккордеон
+			if (self.classList.contains('open')) {
+				control.setAttribute('aria-expanded', true);
+				content.setAttribute('aria-hidden', false);
+				content.style.maxHeight = content.scrollHeight + 'px';
+			} else {
+				control.setAttribute('aria-expanded', false);
+				content.setAttribute('aria-hidden', true);
+				content.style.maxHeight = null;
+			}
+		});
+	});
+});
+
